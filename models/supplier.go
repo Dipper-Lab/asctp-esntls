a2a42a64abda41e62019a4a0f5c7984ed12127ff
Kwasi Manu Biritwum-Nyarko <kwasimbnyarko@gmail.com>
2023-10-12 14:09:12 +0000

    supplier loaction

--- models/supplier.go
+++ models/supplier.go
@@ -10,7 +10,7 @@ type Supplier struct {
 	Id         primitive.ObjectID `bson:"_id" validate:"required"`
 	Name       string             `bson:"name" json:"name" validate:"required,min=5,max=15"`
 	Contact    string             `bson:"contact" json:"contact" validate:"required,min=10,max=10"`
-	ProduceIds []string           `bson:"produce" json:"produce"`
+	Location   string             `bson:"location" json:"location"`
 	SupplierId string             `bson:"supplier_id" json:"supplier_id" validate:"required"`
 	CreatedAt  time.Time          `bson:"created_at" json:"created_at"`
 	UpdatedAt  time.Time          `bson:"updated_at" json:"updated_at"`
