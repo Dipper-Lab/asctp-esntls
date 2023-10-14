0532c0ba78a17a96fb2ad9ef5a224a2af6af4f3f
Kwasi Manu Biritwum-Nyarko <kwasimbnyarko@gmail.com>
2023-10-14 08:47:36 +0000

    user model updates

--- models/user.go
+++ models/user.go
@@ -9,6 +9,7 @@ type User struct {
 	Username     string    `bson:"username" json:"username" validate:"required,min=5,max=50"`
 	Password     string    `bson:"password" json:"password" validate:"required"`
 	Role         string    `bson:"role" json:"role" validate:"required"`
+	OrgId        string    `bson:"org_id" json:"org_id"`
 	Facility     string    `bson:"facility" json:"facility" validate:"required"`
 	FirstName    *string   `bson:"first_name" json:"first_name" validate:"required,min=2,max=50"`
 	LastName     *string   `bson:"last_name" json:"last_name" validate:"required,min=2,max=50"`
@@ -16,6 +17,7 @@ type User struct {
 	Phone        string    `bson:"phone" json:"phone" validate:"required,min=10,max=10"`
 	Token        *string   `bson:"token" json:"token"`
 	RefreshToken *string   `bson:"refresh_Token" json:"refresh_Token"`
+	IsActive     bool      `bson:"isActive" json:"isActive"`
 	CreatedAt    time.Time `bson:"created_At" json:"created_At" validate:"required"`
 	UpdatedAt    time.Time `bson:"updated_At" json:"updated_At" validate:"required"`
 }
