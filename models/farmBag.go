acd09c3e8f48e6c89f3803dd190741d3a6dbccb8
Kwasi Manu Biritwum-Nyarko <kwasimbnyarko@gmail.com>
2023-10-20 11:48:00 +0000

    replaced supplier and product ids in farmBag model with purchase id

--- models/farmBag.go
+++ models/farmBag.go
@@ -5,11 +5,10 @@ import "time"
 type FarmBag struct {
 	Id              string     `bson:"_id" validate:"required"`
 	BagNumber       string     `bson:"bag_number" json:"bag_number"` //format : MMDDYYXXXX
-	ProduceId       string     `bson:"produce_id" json:"produce_id"`
+	PurchaseId      string     `bson:"purchase_id" bson:"purchase_id"`
 	Weight          float32    `bson:"weight" json:"weight"`
 	Price           float32    `bson:"price" json:"price"`
 	MoistureHistory []Moisture `bson:"moisture_history" json:"moisture_history"`
-	SupplierId      string     `bson:"supplier_id" json:"supplier_id"`
 	Status          string     `bson:"status" json:"status"` //unused, purchased, batched
 	BatchId         string     `bson:"batch_id" json:"batch_id"`
 	FarmBagId       string     `bson:"farmBag_id" json:"farmBag_id"`
