baf821e560147e5e536e1cd3f1449602d78a8722
Kwasi Manu Biritwum-Nyarko <kwasimbnyarko@gmail.com>
2023-10-20 11:05:45 +0000

    purchase model supplier and produce id fixes

--- models/purchase.go
+++ models/purchase.go
@@ -9,8 +9,8 @@ import (
 type Purchase struct {
 	Id            primitive.ObjectID `bson:"_id"`
 	Name          string             `bson:"name"   json:"name"`
-	Produce       string             `bson:"produce"  json:"produce"`
-	Supplier      string             `bson:"supplier" json:"supplier"`
+	ProduceId     string             `bson:"produce_id"  json:"produce_id"`
+	SupplierId    string             `bson:"supplier_id" json:"supplier_id"`
 	BuyerUsername string             `bson:"buyer_username"  json:"buyer_username"`
 	FarmerBagIds  []string           `bson:"farmerBag_ids" json:"farmerBag_ids"`
 	PurchaseId    string             `bson:"purchase_id" json:"purchase_id"`
