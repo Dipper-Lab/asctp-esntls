48cdb2f8ef7d6a23a17abda45fe3194747d72683
Kwasi Manu Biritwum-Nyarko <kwasimbnyarko@gmail.com>
2023-11-03 14:31:17 -0400

    validate role update

--- helpers/rbacHelper.go
+++ helpers/rbacHelper.go
@@ -1,16 +1,19 @@
 package helpers
 
 import (
+	"net/http"
+
 	"github.com/dipper-lab/asctp-esntls/consts"
 	"github.com/gin-gonic/gin"
 	"golang.org/x/exp/slices"
 )
 
-func ValidateRole(c *gin.Context, functionality string) (bool, string) {
+func ValidateRole(c *gin.Context, functionality string) {
 
-	if slices.Contains(consts.RBACMap[c.GetString("role")], functionality) {
-		return true, ""
+	if !slices.Contains(consts.RBACMap[c.GetString("role")], functionality) {
+
+		c.JSON(http.StatusBadRequest, gin.H{"error": "user unauthorized"})
+		return
 	}
 
-	return false, "user unauthorized"
 }
