8a22163c89c98395a01a950de84bd065621088ff
Kwasi Manu Biritwum-Nyarko <kwasimbnyarko@gmail.com>
2023-11-15 16:39:57 +0000

    added email to supplier struct

--- models/supplier.go
+++ models/supplier.go
@@ -10,6 +10,7 @@ type Supplier struct {
 	Id         primitive.ObjectID `bson:"_id" validate:"required"`
 	Name       string             `bson:"name" json:"name" validate:"required,min=5,max=15"`
 	Contact    string             `bson:"contact" json:"contact" validate:"required,min=10,max=10"`
+	Email      string             `bson:"email" json:"email" validate:"email"`
 	Location   string             `bson:"location" json:"location"`
 	SupplierId string             `bson:"supplier_id" json:"supplier_id" validate:"required"`
 	ProduceIds []string           `bson:"produce_ids" json:"produce_ids"`
