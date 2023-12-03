7c90b4f01861eafce08136813b077c19d9036bd2
LorenzoAppiadu <92297186+LorenzoAppiadu@users.noreply.github.com>
2023-12-03 10:29:32 +0000

    Update smartSensor.go

--- models/smartSensor.go
+++ models/smartSensor.go
@@ -1,10 +1,15 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type SensorReadings struct {
-	Temperature float32   `bson:"temperature" json:"temperature"`
-	Humidity    float32   `bson:"humidity" json:"humidity"`
-	Location    string    `bson:"location"  json:"location"`
-	UpdatedAt   time.Time `bson:"updated_at" json:"updated_at"`
+	Id          primitive.ObjectID `bson:"_id"`
+	Temperature float32            `bson:"temperature" json:"temperature"`
+	Humidity    float32            `bson:"humidity" json:"humidity"`
+	Location    string             `bson:"location"  json:"location"`
+	UpdatedAt   time.Time          `bson:"updated_at" json:"updated_at"`
 }
