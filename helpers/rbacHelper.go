0b7068ef4cb7797933544d3b80f19380b51891f5
Kwasi Manu Biritwum-Nyarko <kwasimbnyarko@gmail.com>
2023-12-11 15:10:18 +0000

    allowing ADIMN to access all controllers

--- helpers/rbacHelper.go
+++ helpers/rbacHelper.go
@@ -10,6 +10,10 @@ import (
 
 func ValidateRole(c *gin.Context, functionality string) bool {
 
+	if c.GetString("role") == "ADMIN" {
+		return true
+	}
+
 	if !slices.Contains(consts.RBACMap[c.GetString("role")], functionality) {
 
 		c.JSON(http.StatusBadRequest, gin.H{"error": "user unauthorized"})
